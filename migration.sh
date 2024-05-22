3a07f6f76f0652bc32dee1fe75eaa426065ba7b4
JuanSeZ <juan.zanelli@ing.austral.edu.ar>
2024-05-22 20:04:02 -0300

    initial commit

---
+++ migration.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+container_names=("tower" "delivery" "store" "warehouse")
+
+for container_name in "${container_names[@]}"; do
+    if docker inspect -f '{{.State.Running}}' "$container_name" >/dev/null 2>&1; then
+        echo "Docker container $container_name is running."
+        
+        docker exec "$container_name" npx prisma migrate dev
+    else
+        echo "Docker container $container_name is not running."
+    fi
+done
